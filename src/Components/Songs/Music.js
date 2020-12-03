97a6cdc1f545d0ce6eb15d25bdb18d423523831d
ikhuohoneboreimethankgod@gmail.com <calebikhuohon@gmail.com>
2020-12-03 14:32:55 +0100

    fetching data from a musice api

---
+++ src/Components/Songs/Music.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import axios from 'axios';
+
+
+const API = {
+    method: 'GET',
+    url: 'https://musicapp8.p.rapidapi.com/',
+    params: {q: 'test'},
+    headers: {
+      'x-rapidapi-key': '5e41c2dd6cmsh97a4f9e20ffa6e7p15e915jsn9a92371eeeb0',
+      'x-rapidapi-host': 'musicapp8.p.rapidapi.com'
+    }
+  };
+  
+  axios.request(API).then(function (API) {
+      //response are logged in the console
+    console.log(API.data);
+}).catch(function (error) {
+    console.error('No data fetched' +error);
+});
+const Music = () => {
+    return(
+        <div>
+         <h1>Response</h1>
+         <div className="response__data">
+         </div>
+        </div>
+    );
+}
+
+export default Music;
